3be76cf65a70ea9ce36a8d00762a4365ca08cf67
David Tabor <david.tabor@icloud.com>
2022-02-18 13:02:02 -0500

    gulp changes, test css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-console */
 const gulp = require("gulp");
+const postcss = require("gulp-postcss");
 const htmlmin = require("gulp-htmlmin");
 const cleanCSS = require("gulp-clean-css");
 const inlinesource = require("gulp-inline-source");
+const cssnano = require("cssnano");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
 const path = require("path");
 const del = require("del");
 
@@ -19,6 +23,15 @@ const html = () =>
     )
     .pipe(gulp.dest("dist"));
 
+const scss = () =>
+  gulp
+    .src("src/styles/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss(cssnano))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("dist/styles"));
+
 const css = () =>
   gulp
     .src("src/styles/*.css")
@@ -30,7 +43,7 @@ const css = () =>
     )
     .pipe(gulp.dest("dist/styles"));
 
-const purge = () => del(["dist/styles"]);
+// const purge = () => del(["dist/styles"]);
 
 const assets = () => gulp.src("public/*").pipe(gulp.dest("dist"));
 
@@ -38,10 +51,11 @@ const dev = () =>
   gulp.watch(
     ["src/**/*"],
     { ignoreInitial: false },
-    gulp.series(css, html, purge, assets)
+    gulp.series(scss, html, assets)
   );
 
 exports.html = html;
 exports.css = css;
+exports.scss = scss;
 exports.dev = dev;
-exports.default = gulp.series(css, html, purge, assets);
+exports.default = gulp.series(scss, html, assets);
